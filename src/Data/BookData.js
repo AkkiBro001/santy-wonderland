21c41752dfb4bc022dc9c8c6a4498e98246af57b
Akshay <akshay.design4art@gmail.com>
2023-12-22 06:12:25 +0530

    init2

---
+++ src/Data/BookData.js
@@ -0,0 +1,31 @@
+import programmer1 from "../assets/books/Wish-from-Santa-and-the-Programmer.jpg"
+import programmer2 from "../assets/books/Wish-from-Santa-and-the-Programmer-2.jpg"
+import realisticRequests from "../assets/books/Santa-Realistic-Requests-A-Dragon-or-a-Boyfriend.jpg"
+import SantaCrossword from "../assets/books/Santa-and-Crossword.jpg"
+export const BookData = [
+    {
+        name: "Wish from Santa and the Programmer",
+        img: programmer1,
+        desc: "Witty exchanges between Santa and the programmer.",
+        price: 10,
+
+    },
+    {
+        name: "Wish from Santa and the Programmer 2",
+        img: programmer2,
+        desc: "Witty exchanges between Santa and the programmer (Part 2).",
+        price: 10,
+    },
+    {
+        name: "Santa's Realistic Requests: A Dragon or a Boyfriend ?",
+        img: realisticRequests,
+        desc: "The most amusing contemplation, balancing mythical creatures and matters of the heart.",
+        price: 10,
+    },
+    {
+        name: "Santa and Crossword",
+        img: SantaCrossword,
+        desc: "Santa tackles the crossword puzzle.",
+        price: 10,
+    }
+]
